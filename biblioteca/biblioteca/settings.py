0d246967348805ab31a5c77dd849412370913c2c
Maludiane Mamede <maludiane@gmail.com>
2024-08-24 17:23:44 -0300

    CRUD de Biblioteca

--- biblioteca/biblioteca/settings.py
+++ biblioteca/biblioteca/settings.py
@@ -29,18 +29,18 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
+ 
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
     # Django REST framework
-    "rest_framework",
+    'rest_framework',
     # Toys application
-    "core.apps.CoreConfig",
+    'core.apps.CoreConfig',
 ]
 
 MIDDLEWARE = [
