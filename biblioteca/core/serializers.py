85f92f1b41d758204430889f4be56fe98099d6c2
Maludiane Mamede <maludiane@gmail.com>
2024-11-21 17:45:39 -0300

    Atualizacao e implementacao Serializers

--- biblioteca/core/serializers.py
+++ biblioteca/core/serializers.py
@@ -1,44 +1,63 @@
 from rest_framework import serializers
-from .models import Categoria, Autor, Livro
+from .models import Categoria, Autor, Livro, Colecao
+from django.contrib.auth.models import User
 
-class CategoriaSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    nome = serializers.CharField(max_length=100)
-    
-    def create(self, validated_data):
-        return Categoria.objects.create(**validated_data)
-    
-    def update(self, instance, validated_data):
-        instance.nome = validated_data.get('nome', instance.nome)
-        instance.save()
-        return instance
-    
-class AutorSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    nome = serializers.CharField(max_length=100)
-    
-    def create(self, validated_data):
-        return Autor.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
-        instance.nome = validated_data.get('nome', instance.nome)
-        instance.save()
-        return instance
-    
-class LivroSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    titulo = serializers.CharField(max_length=200)
-    autor = serializers.PrimaryKeyRelatedField(queryset=Autor.objects.all())
-    categoria = serializers.PrimaryKeyRelatedField(queryset=Categoria.objects.all())
-    publicado_em = serializers.DateField()
+from rest_framework import serializers
+from .models import Categoria, Autor, Livro, Colecao
 
-    def create(self, validated_data):
-        return Livro.objects.create(**validated_data)
-    
-    def update(self, instance, validated_data):
-        instance.titulo = validated_data.get('titulo', instance.titulo)
-        instance.autor = validated_data.get('autor', instance.autor)
-        instance.categoria = validated_data.get('categoria', instance.categoria)
-        instance.publicado_em = validated_data.get('publicado_em', instance.publicado_em)
-        instance.save()
-        return instance
+
+class CategoriaSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Categoria
+        fields = ["id", "nome"]
+
+
+class AutorSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Autor
+        fields = ["id", "nome"]
+
+
+class LivroSerializer(serializers.HyperlinkedModelSerializer):
+    autor = serializers.SlugRelatedField(
+        queryset=Autor.objects.all(), slug_field="name"
+    )
+    categoria = serializers.SlugRelatedField(
+        queryset=Categoria.objects.all(), slug_field="name"
+    )
+
+    class Meta:
+        model = Livro
+        fields = [
+            "id",
+            "titulo",
+            "autor",
+            "categoria",
+            "publicado_em",
+        ]
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+            "email",
+        ]
+
+
+class ColecaoSerializer(serializers.ModelSerializer):
+    livros = LivroSerializer(many=True)
+    colecionador = UserSerializer()
+
+    class Meta:
+        model = Colecao
+        fields = [
+            "id",
+            "nome",
+            "descricao",
+            "livros",
+            "colecionador",
+        ]
