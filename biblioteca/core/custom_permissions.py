bfc82eb115b9fd8bceb6c597d95036f0cfcef104
Maludiane Mamede <maludiane@gmail.com>
2024-11-27 21:04:54 -0300

    Atividade Auth, Doc e test

--- biblioteca/core/custom_permissions.py
+++ biblioteca/core/custom_permissions.py
@@ -1,8 +1,11 @@
 from rest_framework import permissions
 
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
+class IsCurrentUserOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
+            
             return True
-        return obj.colecionador == request.user
+        else:
+            
+            return obj.owner == request.user
